3e529a25f697753b69fadd60249346efdf0a8af3
Ishara Induranga <69160726+isharainduranga@users.noreply.github.com>
2022-09-05 06:29:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/xylophone/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.xylophone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.view.View;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Helpful Constants
+    private final int NR_OF_SIMULTANEOUS_SOUNDS = 7;
+    private final float LEFT_VOLUME = 1.0f;
+    private final float RIGHT_VOLUME = 1.0f;
+    private final int NO_LOOP = 0;
+    private final int PRIORITY = 0;
+    private final float NORMAL_PLAY_RATE = 1.0f;
+
+    // TODO: Add member variables here
+    private SoundPool mSoundPool;
+    private int mCSoundId;
+    private int mDSoundId;
+    private int mESoundId;
+    private int mFSoundId;
+    private int mGSoundId;
+    private int mHSoundId;
+    private int mISoundId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO: Create a new SoundPool
+        mSoundPool=new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS,AudioManager.STREAM_MUSIC,0);
+
+        // TODO: Load and get the IDs to identify the sounds
+        mCSoundId=mSoundPool.load(getApplicationContext(),R.raw.note1_c,1);
+        mDSoundId=mSoundPool.load(getApplicationContext(),R.raw.note2_d,1);
+        mESoundId=mSoundPool.load(getApplicationContext(),R.raw.note3_e,1);
+        mFSoundId=mSoundPool.load(getApplicationContext(),R.raw.note4_f,1);
+        mGSoundId=mSoundPool.load(getApplicationContext(),R.raw.note5_g,1);
+        mHSoundId=mSoundPool.load(getApplicationContext(),R.raw.note6_h,1);
+        mISoundId=mSoundPool.load(getApplicationContext(),R.raw.note7_i,1);
+
+
+    }
+
+    // TODO: Add the play methods triggered by the buttons
+    public void playC(View v){
+//        Log.d("Xylohone","Red clicked");
+        mSoundPool.play(mCSoundId, LEFT_VOLUME, RIGHT_VOLUME,PRIORITY,NO_LOOP, NORMAL_PLAY_RATE);
+    }
+    public void playD(View v){
+        mSoundPool.play(mDSoundId, LEFT_VOLUME, RIGHT_VOLUME,PRIORITY,NO_LOOP, NORMAL_PLAY_RATE);
+
+    }
+    public void playE(View v){
+        mSoundPool.play(mESoundId, LEFT_VOLUME, RIGHT_VOLUME,PRIORITY,NO_LOOP, NORMAL_PLAY_RATE);
+
+    }
+    public void playF(View v){
+        mSoundPool.play(mFSoundId, LEFT_VOLUME, RIGHT_VOLUME,PRIORITY,NO_LOOP, NORMAL_PLAY_RATE);
+
+    }
+    public void playG(View v){
+        mSoundPool.play(mGSoundId, LEFT_VOLUME, RIGHT_VOLUME,PRIORITY,NO_LOOP, NORMAL_PLAY_RATE);
+
+    }
+    public void playH(View v){
+        mSoundPool.play(mHSoundId, LEFT_VOLUME, RIGHT_VOLUME,PRIORITY,NO_LOOP, NORMAL_PLAY_RATE);
+
+    }
+    public void playI(View v){
+        mSoundPool.play(mISoundId, LEFT_VOLUME, RIGHT_VOLUME,PRIORITY,NO_LOOP, NORMAL_PLAY_RATE);
+
+    }
+}
